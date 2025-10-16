a4dab355b6595e7ab3fef6332a46cee0e8ba0fc3
sameerasw <sameerasw.com@gmail.com>
2025-10-16 21:32:32 +0530

    Initial commit

---
+++ week-02/ParcelAppCalculator/ParcelAppCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParcelAppCalculator
+//
+//  Created by Sameera Sandakelum on 2025-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
